3b7bbfde7c5cc1b181a2f1355cec4a9151c63406
Marco Yang <marcoyang123@gmail.com>
2022-01-16 17:05:12 -0500

    Updates

---
+++ component---src-pages-projects-js-a4fe66dff5b70716d894.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"16l3":function(e,t,n){"use strict";n.r(t);var l=n("q1tI"),a=n.n(l),c=n("Bl7J"),r=n("vrFN");t.default=function(){return a.a.createElement(c.a,null,a.a.createElement(r.a,{title:"Projects"}),a.a.createElement("h1",null,"projects"),a.a.createElement("p",null,"some cool things"))}}}]);
+//# sourceMappingURL=component---src-pages-projects-js-a4fe66dff5b70716d894.js.map
