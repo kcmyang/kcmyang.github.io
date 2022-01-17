bcea722319600fb7fdaa2e0a5c875bb530df9be4
Marco Yang <marcoyang123@gmail.com>
2022-01-16 21:13:15 -0500

    Updates

---
+++ component---src-pages-404-tsx-6079c30443293ba470f6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[218],{938:function(e,t,n){n.r(t);n(7294);var r=n(2266),h=n(2722),i=n(611);t.default=function(){return(0,i.BX)(r.Z,{children:[(0,i.tZ)(h.Z,{title:"404: Not found"}),(0,i.tZ)("h1",{style:{color:"#ff6188"},children:"404: Not Found"}),(0,i.tZ)("p",{children:"There doesn't seem to be anything here... :("})]})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-6079c30443293ba470f6.js.map
